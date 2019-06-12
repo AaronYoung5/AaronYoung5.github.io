0e8b0f39cbe73a099b0d7222a3c9b31fd1c0aa3c
aryoung5 <aryoung5@wisc.edu>
2019-06-12 08:20:34 -0700

    added website

--- README.md
+++
@@ -1,2 +0,0 @@
-# AaronYoung5.github.io
-My personal website.
--- script.js
+++ script.js
@@ -1,59 +1,137 @@
+var myAnimatedHeader = function() {
 
-const gsTitle = {
-	init() {
-		this._root = document.querySelector( "#gsTitle" );
-		this._titles = this._root.querySelectorAll( ".gsTitle-title" );
-		this._frame = this._frame.bind( this );
-		this.setTexts( [
-			"Coming Soon !",
-			"cOMING sORLD .",
-			"ɔ0w˥ug $0øµ ¡",
-			"¢omluℊ ∑θθℿ ¡",
-			"こんにちはん 世界国地 ¡",
-			"ɔøɯᴉυƃ Ƨøøυ",
-		] );
-	},
-	on() {
-		if ( !this._frameId ) {
-			this._frame();
-		}
-	},
-	off() {
-		clearTimeout( this._frameId );
-		this._textContent( this._text );
-		delete this._frameId;
-	},
-	setTexts( [ text, ...alt ] ) {
-		this._text = text;
-		this._textAlt = alt;
-	},
+  var docElem = document.documentElement,
+    headder = document.querySelector('.navbar-default'),
+    didScroll = false,
+    changeHeaderOn = 0;
 
-	// private:
-	_text: "",
-	_textAlt: [],
-	_rand( n ) {
-		return Math.random() * n | 0;
-	},
-	_textContent( txt ) {
-		this._titles.forEach( el => el.textContent = txt );
-	},
-	_frame() {
-		const txt = Array.from( this._text );
+  function init() {
+    window.addEventListener('scroll', function(event) {
+      if (!didScroll) {
+        didScroll = true;
+        setTimeout(scrollPage, 0);
+      }
+    }, false);
+  }
 
-		for ( let i = 0; i < 6; ++i ) {
-			const ind = this._rand( this._text.length );
+  function scrollPage() {
+    var sy = scrollY();
+    if (sy >= changeHeaderOn) {
+      $('.navbar').addClass('navbar-shrink');
+    } else {
+      $('.navbar').removeClass('navbar-shrink');
+    }
+    didScroll = false;
+  }
 
-			txt[ ind ] = this._textAlt[ this._rand( this._textAlt.length ) ][ ind ];
-		}
-		this._textContent( txt.join( "" ) );
-		this._root.classList.add( "gsTitle-glitched" );
-		setTimeout( () => {
-			this._textContent( this._text );
-			this._root.classList.remove( "gsTitle-glitched" );
-		}, 50 + Math.random() * 200 );
-		this._frameId = setTimeout( this._frame, 250 + Math.random() * 500 );
-	},
-};
+  function scrollY() {
+    return window.pageYOffset || docElem.scrollTop;
+  }
 
-gsTitle.init();
-gsTitle.on();
+  init();
+}();
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        if (target.selector == "#wisconsin-autonomous" || target.selector == "#insight-wisconsin" || target.selector == "#sbel" || target.selector == "#engineering-expo") {
+          $('html, body').animate({
+              scrollTop: target.offset().top - 75
+            },
+            1000);
+        } else {
+          $('html, body').animate({
+              scrollTop: target.offset().top
+            },
+            1000);
+        }
+
+        return false;
+      }
+    }
+  });
+});
+
+
+// hide #back-top first
+$("#back-top").hide();
+
+// fade in #back-top
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('#back-top').fadeIn();
+  } else {
+    $('#back-top').fadeOut();
+  }
+});
+
+// Close the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  $('.navbar-toggle:visible').click();
+});
+
+// Dots
+$(".dots").each(function(i) {
+  var ii = 0;
+  var percent = $(this).data('percent');
+  while (ii < 5) {
+    if (ii < percent) {
+      $(this).append("<li class='active'></li>");
+    } else {
+      $(this).append('<li></li>');
+    }
+    ii++;
+  }
+  this.style.color = "";
+});
+
+// Email form submission
+$(function () {
+
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+    $('#contactForm').validator();
+
+
+    // when the form is submitted
+    $('#contactForm').on('submit', function (e) {
+      console.log("Submitting");
+
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+            var url = "mail.php";
+
+            // POST values in the background the the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    // data = JSON object that contact.php returns
+
+                    // we recieve the type of the message: success x danger and apply it to the
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+                    // If we have messageAlert and messageText
+                    if (messageAlert && messageText) {
+                        // inject the alert to .messages div in our form
+                        $('#contactForm').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contactForm')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
