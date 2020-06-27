b1c32148dd90ed60f11e3492828fc7caf084c217
aryoung5 <aryoung5@wisc.edu>
2020-06-27 11:19:39 -0500

    Should have fixed a video issue

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var myAnimatedHeader = function() {
         didScroll = true;
         setTimeout(scrollPage, 0);
       }
-    }, false);
+    }, !1);
   }
 
   function scrollPage() {
@@ -89,48 +89,48 @@ $(".dots").each(function(i) {
 });
 
 // Email form submission
-$(function () {
+$(function() {
 
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
+  // init the validator
+  // validator files are included in the download package
+  // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-    $('#contactForm').validator();
+  $('#contactForm').validator();
 
 
-    // when the form is submitted
-    $('#contactForm').on('submit', function (e) {
-      console.log("Submitting");
+  // when the form is submitted
+  $('#contactForm').on('submit', function(e) {
+    console.log("Submitting");
 
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "mail.php";
-            // POST values in the background the the script URL
-            $.ajax({
-                // type: "POST",
-                // url: url,
-                // data: $(this).serialize()
-                // success: function (data)
-                // {
-                //     // data = JSON object that contact.php returns
-                //
-                //     // we recieve the type of the message: success x danger and apply it to the
-                //     var messageAlert = 'alert-' + data.type;
-                //     var messageText = data.message;
-                //
-                //     // let's compose Bootstrap alert box HTML
-                //     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                //
-                //     // If we have messageAlert and messageText
-                //     if (messageAlert && messageText) {
-                //         // inject the alert to .messages div in our form
-                //         $('#contactForm').find('.messages').html(alertBox);
-                //         // empty the form
-                //         $('#contactForm')[0].reset();
-                //     }
-                // }
-            });
-            return false;
-        }
-    })
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+      var url = "mail.php";
+      // POST values in the background the the script URL
+      $.ajax({
+        // type: "POST",
+        // url: url,
+        // data: $(this).serialize()
+        // success: function (data)
+        // {
+        //     // data = JSON object that contact.php returns
+        //
+        //     // we recieve the type of the message: success x danger and apply it to the
+        //     var messageAlert = 'alert-' + data.type;
+        //     var messageText = data.message;
+        //
+        //     // let's compose Bootstrap alert box HTML
+        //     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+        //
+        //     // If we have messageAlert and messageText
+        //     if (messageAlert && messageText) {
+        //         // inject the alert to .messages div in our form
+        //         $('#contactForm').find('.messages').html(alertBox);
+        //         // empty the form
+        //         $('#contactForm')[0].reset();
+        //     }
+        // }
+      });
+      return false;
+    }
+  })
 });
